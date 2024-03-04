c1ab59bad9b9dfb92d05cba5311288cd8946fe2e
Crowdbotics <team@crowdbotics.com>
2024-03-04 15:11:24 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ubadada-46786",
+  url: "https://ubadada-46786.botics.co",
+  api: "https://ubadada-46786.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
